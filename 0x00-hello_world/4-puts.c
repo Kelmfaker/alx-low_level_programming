cd301507ce3129122348e21850abf0b408cdd9dc
Kelmfaker <kelmfaker@gmail.com>
2024-09-02 15:38:49 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+/**
+ *main - this is the main function
+ *
+ *Return: always 0
+ */
+puts("\""Programming is like building a multilingual puzzle);
 return (0);
 }

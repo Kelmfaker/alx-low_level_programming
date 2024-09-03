23522aef3ebd5a4589b9fafd86ecc17af95388e4
Kelmfaker <kelmfaker@gmail.com>
2024-09-02 21:07:46 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+/**
+ * main - this is the main function
+ * Description: this function prints exactely a text given
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-/**
- *main - this is the main function
- *
- *Return: always 0
- */
-puts("\""Programming is like building a multilingual puzzle);
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }

f9d717c6946180660bea6f34425b2e821f67b712
Kelmfaker <kelmfaker@gmail.com>
2024-09-03 21:14:07 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Description: This function will prints the size of various types
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+printf("Size of a char: " + sizeof(char) + " byte(s)\n");
+printf("Size of a int: " + sizeof(int) + " byte(s)\n");
+printf("Size of a long int: " + sizeof(long int) + " byte(s)\n");
+printf("Size of a long long int: " + sizeof(long long int) + " byte(s)\n");
+printf("Size of a float: " + sizeof(float) + " byte(s)\n");
+}

c0c6ec81df9f2cc2074897dd7f6430468ba04c56
Kelmfaker <kelmfaker@gmail.com>
2024-09-06 13:13:47 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit <= 9; digit++)
+{
+printf("%d", digit);
+}
+printf("\n");
+return (0);
+}

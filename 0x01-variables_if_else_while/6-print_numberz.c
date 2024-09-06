d27ec8e4ed7e1969d44a9b12b85c484cd60f6068
Kelmfaker <kelmfaker@gmail.com>
2024-09-06 13:37:07 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit <= 9; digit++)
+{
+putchar(digit);
+}
+putchar('\n');
+return (0);
+}

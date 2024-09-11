242b60a9e833642fb84c82a04d0c51e73a9e9281
Kelmfaker <kelmfaker@gmail.com>
2024-09-11 04:32:57 -0700

    abc

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - main that return the absolate value
+ *
+ * @n: parameter to be tested
+ * Return: value
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+	return (-n);
+else
+	return (n);
+}

9fc6604885e029da0e54841bbf9e965282bebec8
Kelmfaker <kelmfaker@gmail.com>
2024-09-11 18:32:24 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - function prints the result of two numbers
+ * @i: first parameter
+ * @j: second parameter
+ *
+ * Return: always i + j
+ */
+
+
+int add(int i, int j)
+{
+return (i + j);
+}

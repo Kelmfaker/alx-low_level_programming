1bd3825e3af3657d58c40ea30a126c02608cdbc6
Kelmfaker <kelmfaker@gmail.com>
2024-09-11 19:29:30 -0700

    prints to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,24 +10,24 @@
 
 void print_to_98(int n)
 {
-while (n <= 98)
-{
-printf("%d", n);
-if (n != 98)
-printf(", ");
-n++;
-}
-printf("\n");
-while (n >= 98)
-{
-printf("%d", n);
-if (n != 98)
-printf(", ");
-n--;
-}
-printf("\n");
-while (n == 98)
-{
-printf("%d\n", n);
-}
+	while (n <= 98)
+	{
+	printf("%d", n);
+	if (n != 98)
+	printf(", ");
+	n++;
+	}
+	printf("\n");
+		while (n >= 98)
+		{
+		printf("%d", n);
+		if (n != 98)
+		printf(", ");
+		n--;
+		}
+		printf("\n");
+			while (n == 98)
+			{
+			printf("%d\n", n);
+			}
 }

e8bff4ca009235ae1988c2363d7cbd4546c8478a
Kelmfaker <kelmfaker@gmail.com>
2024-09-14 14:18:32 -0700

    positive or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -12,17 +12,14 @@
  * Return: always 0
  */
 
-int main(void)
+void positive_or_negative(int i)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+
 /* your code goes there */
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i == 0)
+printf("%d is zero\n", i);
 else
-printf("%d is negative\n", n);
-return (0);
+printf("%d is negative\n", i);
 }

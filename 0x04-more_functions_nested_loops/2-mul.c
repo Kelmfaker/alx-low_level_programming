cf06e9d298209a0dc745a7ce32400db789823b90
Kelmfaker <kelmfaker@gmail.com>
2024-09-15 09:57:31 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - function that multiple to integer
+ * @a : fist parameter
+ * @b : second parameter
+ *
+ * Return: the result
+ */
+
+int mul(int a, int b)
+{
+int m;
+
+m = a * b;
+	return (m);
+}
